2c323e0884eb8ab374ff889dda36c4f301f32157
Sam Naylor <s.naylor5@newcastle.ac.uk>
2023-04-20 18:11:19 +0100

    make it more easily extenable

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from math import sqrt, sin, cos, radians
+import random
+
+from math import cos, sin, sqrt, radians
 from heapq import heappop, heappush
-from typing import Generic, TypeVar, Any
 from dataclasses import dataclass
 
 import pygame
 
 pygame.init()
+pygame.font.init()
+
+# region Constants
 
 hex_size = 30
 hex_width = sqrt(3) * hex_size
@@ -13,230 +17,259 @@ hex_height = hex_size * 3 / 2
 hex_spacing_horizontal = hex_width
 hex_spacing_vertical = hex_height
 
+window_width = 640
+window_height = 480
+render_offset = (-(window_width // 2), -(window_height // 2))
 
-T = TypeVar("T")
+gamefont = pygame.font.SysFont("Helvetica", 24, False, False)
+
+# endregion
 
 
-class Singleton(Generic[T], type):
-    def __init__(self: "Singleton[T]", __name: str, __bases: tuple[type, ...], __dict: dict[str, Any]) -> None:
-        super(Singleton, self).__init__(__name, __bases, __dict)
-        self._instance: T = super(Singleton, self).__call__()
-
-    def __call__(self: "Singleton[T]", *args: Any, **kwargs: Any) -> T:
-        return self._instance
-
+# region utilities
 
 def lerp(a: float, b: float, t: float) -> float:
     return a + (b - a) * t
 
+# endregion
+
+
+# region Coordinates
 
 @dataclass(eq=True, order=True, unsafe_hash=True)
 class Axial:
-    " axial coordinates "
-
     q: float
     r: float
 
-    @staticmethod
-    def round(axial: "Axial") -> "Axial":
-        q = round(axial.q)
-        r = round(axial.r)
-        s = round(-(axial.q + axial.r))
+    @property
+    def neighbours(self) -> list["Axial"]:
+        return [(self + t) for t in [Axial(1, 0), Axial(1, -1), Axial(0, -1), Axial(-1, 0), Axial(-1, 1), Axial(0, 1)]]
 
-        dq = abs(q - axial.q)
-        dr = abs(r - axial.r)
-        ds = abs(s - -(axial.q + axial.r))
+    def dist(self, coords: "Axial") -> float:
+        diff = self - coords
+        return (abs(diff.q) + abs(diff.q + diff.r) + abs(diff.r)) / 2
+
+    def lerp(self, coords: "Axial", t: float) -> "Axial":
+        return Axial(lerp(self.q, coords.q, t), lerp(self.r, coords.r, t))
+
+    def neighbour(self, i: int) -> "Axial":
+        if (i < 0) or (i > 5):
+            raise RuntimeError(f"neighbour index must be in range 0 - 5, not {i}")
+
+        return self.neighbours[i]
+
+    def round(self) -> "Axial":
+        q, r, s = round(self.q), round(self.r), round(-(self.q + self.r))
+        dq, dr, ds = abs(q - self.q), abs(r - self.r), abs(s - -(self.q + self.r))
 
         if (dq > dr) and (dq > ds):
             q = -(r + s)
         elif (dr > ds):
             r = -(q + s)
 
-        return Axial(q, r)
-
-    @staticmethod
-    def pixel_to_hex(point: tuple[float, float]) -> "Axial":
-        px, py = point[0] - 320, point[1] - 240
-        q = (sqrt(3) / 3 * px - 1 / 3 * py) / hex_size
-        r = (2 / 3 * py) / hex_size
-
-        return Axial.round(Axial(q, r))
-
-    @staticmethod
-    def direction(i: int) -> "Axial":
-        return [Axial(1, 0), Axial(1, -1), Axial(0, -1), Axial(-1, 0), Axial(-1, 1), Axial(0, 1)][i]
-
-    def add(self, other: "Axial") -> "Axial":
-        return Axial(self.q + other.q, self.r + other.r)
-
-    def sub(self, other: "Axial") -> "Axial":
-        return Axial(self.q - other.q, self.r - other.r)
-
-    def neighbour(self, i: int) -> "Axial":
-        return self.add(Axial.direction(i))
-
-    def distance(self, other: "Axial") -> float:
-        diff = self.sub(other)
-        return (abs(diff.q) + abs(diff.q + diff.r) + abs(diff.r)) / 2
-
-    def lerp(self, other: "Axial", t: float) -> "Axial":
-        return Axial(lerp(self.q, other.q, t), lerp(self.r, other.r, t))
-
-    def line(self, other: "Axial") -> list["Axial"]:
-        N = self.distance(other)
-        axials: list[Axial] = []
-
-        for i in range(0, round(N)):
-            axials.append(Axial.round(self.lerp(other, 1.0 / N * i)))
-
-        axials.append(other)
-
-        return axials
+        return (q, r)
 
     def to_pixel(self) -> tuple[float, float]:
-        x = hex_size * (sqrt(3) * self.q + sqrt(3) / 2 * self.r) + 320
-        y = hex_size * (3 / 2 * self.r) + 240
+        x = hex_size * (sqrt(3) * self.q + sqrt(3) / 2 * self.r) - render_offset[0]
+        y = hex_size * (3 / 2 * self.r) - render_offset[1]
 
         return (x, y)
 
+    @staticmethod
+    def from_pixel(point: tuple[float, float]) -> "Axial":
+        px = point[0] + render_offset[0]
+        py = point[1] + render_offset[1]
 
-@dataclass(unsafe_hash=True)
-class HexTile:
-    coords: Axial
-    obstacle: bool = False
+        q = (sqrt(3) / 3 * px - 1 / 3 * py) / hex_size
+        r = (2 / 3 * py) / hex_size
 
-    origin: bool = False
-    base_colour: tuple[int, int, int] = (0, 0, 0)
-    origin_colour: tuple[int, int, int] = (0, 0, 255)
-    hover_colour: tuple[int, int, int] = (0, 255, 0)
-    border_colour: tuple[int, int, int] = (255, 255, 255)
-    obstacle_colour: tuple[int, int, int] = (255, 0, 0)
+        return Axial(q, r).round()
 
-    @property
-    def cost(self) -> int:
-        return 999 if self.obstacle else 1
+    def __add__(self, __o: object) -> "Axial":
+        match __o:
+            case Axial(_q, _r):
+                return Axial(self.q + _q, self.r + _r)
 
-    def is_hover(self) -> bool:
-        return self.coords.pixel_to_hex(pygame.mouse.get_pos()) == self.coords
+            case tuple(float(_q), float(_r)):
+                return Axial(self.q + _q, self.r + _r)
 
-    def vertices(self) -> list[tuple[float, float]]:
+            case _:
+                raise RuntimeError()
+
+    def __sub__(self, __o: object) -> "Axial":
+        match __o:
+            case Axial(_q, _r):
+                return Axial(self.q - _q, self.r - _r)
+
+            case tuple(float(_q), float(_r)):
+                return Axial(self.q - _q, self.r - _r)
+
+            case _:
+                raise RuntimeError()
+
+# endregion
+
+
+# region Base Tile
+
+class Tile:
+    """
+    Basic Tile class that implements only the core functionality
+    """
+
+    def __init__(self, coords: Axial) -> None:
+        self.coords = coords
+        self.vertices: list[tuple[float, float]] = []
+
         centerx, centery = self.coords.to_pixel()
-        verts: list[tuple[float, float]] = []
 
         for i in range(6):
             angle = radians(60 * i - 30)
-            verts.append((centerx + hex_size * cos(angle), centery + hex_size * sin(angle)))
+            self.vertices.append((centerx + hex_size * cos(angle), centery + hex_size * sin(angle)))
 
-        return verts
+        self._cost = random.randint(1, 5)
 
-    def reachable(self, goal: Axial, movement: int = 10) -> bool:
-        " Tiles reachable from here within movement tiles "
+    @property  # TODO: make abstract
+    def cost(self) -> int:
+        return self._cost
 
-        state = Game()
+    def is_hovered_over(self) -> bool:
+        return Axial.from_pixel(pygame.mouse.get_pos()) == self.coords
 
-        visited = set[Axial]()
-        visited.add(self.coords)
-        fringes: list[list[Axial]] = [[self.coords]]
+    def distance(self, tile: "Tile") -> float:
+        return self.coords.dist(tile.coords)
 
-        for k in range(1, movement + 1):
-            fringes.append([])
-            for hex in fringes[k - 1]:
-                for direction in range(6):
-                    neighbour = hex.neighbour(direction)
-                    exists = state.has_tile(neighbour)
-                    if exists and ((neighbour not in visited) and (not state.get_tile(neighbour).obstacle)):
-                        visited.add(neighbour)
-                        fringes[k].append(neighbour)
+    def render(self, surface: pygame.Surface) -> None:  # TODO: make abstract
+        pygame.draw.polygon(surface, (0, 0, 255), self.vertices)
+        pygame.draw.polygon(surface, (255, 255, 255), self.vertices, 2)
 
-        return goal in visited
+        text = gamefont.render(f"{self.cost}", True, (255, 255, 255))
+        rect = text.get_rect(center=self.coords.to_pixel())
 
-    def toggle_obstacle(self) -> None:
-        self.obstacle = not self.obstacle
+        surface.blit(text, rect)
 
-    def render(self, surface: pygame.Surface) -> None:
-        colour = self.origin_colour if self.origin else (self.hover_colour if self.is_hover() else self.base_colour)
-        colour = colour if not self.obstacle else self.obstacle_colour
-        verts = self.vertices()
-
-        pygame.draw.polygon(surface, colour, verts)
-        pygame.draw.polygon(surface, self.border_colour, verts, 2)
+# endregion
 
 
-class Game(metaclass=Singleton):
-    def __init__(self) -> None:
-        self.store: dict[tuple[int, int], HexTile] = {}
+# region Tile Functions
 
-        locations: list[tuple[int, int]] = [
-            (0, -3), (1, -3), (2, -3), (3, -3),
-            (-1, -2), (0, -2), (1, -2), (2, -2), (3, -2),
-            (-2, -1), (-1, -1), (0, -1), (1, -1), (2, -1), (3, -1),
-            (-3, 0), (-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0), (3, 0),
-            (-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1),
-            (-3, 2), (-2, 2), (-1, 2), (0, 2), (1, 2),
-            (-3, 3), (-2, 3), (-1, 3), (0, 3)
-        ]
+# TODO: should be an abstract method implemented on the individual tile types?
+def is_tile_reachable(tilemap: dict[Axial, Tile], origin: Tile, goal: Tile, *, movement: int = 10) -> bool:
+    visited = set[Axial]()
+    fringes = [[origin.coords]]
 
-        for (q, r) in locations:
-            self.store[(q, r)] = HexTile(Axial(q, r))
+    visited.add(origin.coords)
 
-    def has_tile(self, coords: Axial) -> bool:
-        return self.store.get((int(coords.q), int(coords.r))) is not None
+    for k in range(1, 100):
+        fringes.append([])
+        for hex in fringes[k - 1]:
+            for direction in range(6):
+                neighbour = hex.neighbour(direction)
+                exists = tilemap.get(neighbour) is not None
 
-    def get_tile(self, coords: Axial) -> HexTile:
-        return self.store[(int(coords.q), int(coords.r))]
+                if exists and (neighbour not in visited):
+                    visited.add(neighbour)
+                    fringes[k].append(neighbour)
+
+    return goal.coords in visited
 
 
-def astar_pathfinding(start: Axial, goal: Axial) -> list[Axial]:
+def draw_path(surface: pygame.Surface, path: list[Tile], colour: tuple[int, int, int]) -> None:
+    points = [tile.coords.to_pixel() for tile in path]
+
+    if len(points) >= 2:
+        pygame.draw.lines(surface, colour, False, points, 3)
+
+# endregion
+
+
+# region A*
+
+def astar_pathfinding(tilemap: dict[Axial, Tile], origin: Tile, goal: Tile, *, movement: int = 10) -> tuple[list[Tile], list[Tile]]:
     def heuristic(a: Axial, b: Axial) -> float:
-        return a.distance(b)
+        return a.dist(b)
 
     def get_neighbours(tile: Axial) -> list[Axial]:
-        return filter(lambda t: Game().has_tile(t), [tile.neighbour(i) for i in range(6)])
+        return filter(lambda t: tilemap.get(t) is not None, [tile.neighbour(i) for i in range(6)])
 
-    state = Game()
-    frontier: list[tuple[float, Axial]] = [(0, start)]
+    origin_ = origin.coords
+    goal_ = goal.coords
+
+    frontier: list[tuple[float, Axial]] = [(0, origin_)]
     came_from = {}
-    cost_so_far = {start: 0}
+    cost_so_far = {origin_: 0}
 
     while frontier:
         _, current = heappop(frontier)
 
-        if current == goal:
+        if current == goal_:
             break
 
         for neighbour in get_neighbours(current):
-            if state.has_tile(neighbour):
-                new_cost = cost_so_far[current] + state.get_tile(neighbour).cost
+            if (tile := tilemap.get(neighbour)) is not None:
+                new_cost = cost_so_far[current] + tile.cost
 
                 if (neighbour not in cost_so_far) or (new_cost < cost_so_far[neighbour]):
                     cost_so_far[neighbour] = new_cost
-                    priority = new_cost + heuristic(goal, neighbour)
+                    priority = new_cost + heuristic(goal_, neighbour)
                     heappush(frontier, (priority, neighbour))
                     came_from[neighbour] = current
 
-    path = [goal]
-    while path[-1] != start:
+    path = [goal_]
+
+    while (path[-1] != origin_):
         path.append(came_from[path[-1]])
+
     path.reverse()
 
-    return path
+    moveable = [path[0]]
+    k = 1
+    while cost_so_far[path[k]] <= movement:
+        moveable.append(path[k])
+        k += 1
 
+        if k >= len(path):
+            break
+
+    return [tilemap[c] for c in moveable], [tilemap[c] for c in path]
+
+# endregion
+
+
+# region entrypoint
 
 def is_quit_event(event: pygame.event.Event) -> bool:
     return (event.type == pygame.QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE))
 
 
+test_tiles = list(map(lambda t: Axial(t[0], t[1]), [
+    (-2, -1), (-1, -1), (0, -1), (1, -1), (2, -1), (3, -1),
+    (-3, 0), (-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0), (3, 0),
+    (-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1),
+]))
+
+
+def generate_tilemap() -> dict[Axial, Tile]:
+    return dict((coord, Tile(coord)) for coord in test_tiles)
+
+
 def main() -> int:
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((window_width, window_height))
     clock = pygame.time.Clock()
     running = True
 
-    black = (0, 0, 0)
-    white = (255, 255, 255)
+    text = gamefont.render("Press enter to re-roll tile costs.", True, (255, 255, 255))
+    rect = text.get_rect(topleft=(20, 20))
 
-    state = Game()
-    origin: HexTile | None = None
-    hovered: HexTile | None = None
+    text2 = gamefont.render("Total movement: 10 (going top left to bottom right)", True, (255, 255, 255))
+    rect2 = text2.get_rect(topleft=(20, 50))
+
+    text3 = gamefont.render("Green - 'moveable', red - 'desired'", True, (255, 255, 255))
+    rect3 = text3.get_rect(topleft=(20, 80))
+
+    tilemap = generate_tilemap()
+    origin = tilemap[Axial(-2, -1)]
+    goal = tilemap[Axial(2, 1)]
 
     while running:
         for event in pygame.event.get():
@@ -244,50 +277,28 @@ def main() -> int:
                 running = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    t = Axial.pixel_to_hex(pygame.mouse.get_pos())
+                if event.key == pygame.K_RETURN:
+                    tilemap = generate_tilemap()
 
-                    if state.has_tile(t):
-                        tile = state.get_tile(t)
-                        if not tile.obstacle:
-                            if origin is not None:
-                                origin.origin = False
+        screen.fill((0, 0, 0))
 
-                            origin = tile
-                            tile.origin = True
+        for (t, r) in [(text, rect), (text2, rect2), (text3, rect3)]:
+            screen.blit(t, r)
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                t = Axial.pixel_to_hex(event.pos)
-
-                if state.has_tile(t):
-                    state.get_tile(t).toggle_obstacle()
-
-        screen.fill(black)
-
-        hovered = None
-        for (_, tile) in state.store.items():
+        for tile in tilemap.values():
             tile.render(screen)
 
-            if tile.is_hover():
-                hovered = tile
-
-        if (origin is not None) and (hovered is not None) and (not hovered.obstacle) and (origin.reachable(hovered.coords)):
-            line_from_origin = astar_pathfinding(origin.coords, hovered.coords)
-            points: list[tuple[float, float]] = []
-
-            for hex in line_from_origin:
-                points.append(hex.to_pixel())
-
-            if len(points) >= 2:
-                pygame.draw.lines(screen, white, False, points, 3)
+        moveable, dream = astar_pathfinding(tilemap, origin, goal, movement=10)
+        draw_path(screen, dream, (255, 0, 0))
+        draw_path(screen, moveable, (0, 255, 0))
 
         pygame.display.flip()
         clock.tick(60)
-
-    pygame.quit()
 
     return 0
 
 
 if __name__ == "__main__":
     raise SystemExit(main())
+
+# endregion
